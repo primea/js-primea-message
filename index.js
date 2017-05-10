cdae8c117002e4c05a3957f50a6dea1ed48d0ce8
wanderer <mjbecze@gmail.com>
2017-05-10 11:00:31 -0400

    updated schema

--- index.js
+++ index.js
@@ -6,31 +6,65 @@ module.exports = class Message {
   /**
    * @param {*} payload what you want to send in a message
    */
-  constructor (payload = {}) {
-    this._payload = payload
+  constructor (opts = {}) {
+    const defaults = {
+      resources: {
+        ticks: 0,
+        priority: 0
+      },
+      data: new ArrayBuffer([]),
+      ports: []
+    }
+
+    Object.assign(defaults.resources, opts.resources)
+    this._opts = Object.assign(defaults, opts)
+
+    // set by the kernel
     this._ticks = 0
+    this._priority = 0
     this._hops = 0
     this._fromPort = null
+    this._fromPortsTicks = 0
   }
 
   /**
    * Returns the messages payload
-   * @returns {*}
+   * @returns {ArrayBuffer}
    */
-  get payload () {
-    return this._payload
-  }
-
-  get fromPort () {
-    return this._fromPort
+  get data () {
+    return this._opts.data
   }
 
   /**
-   * Returns the message's resources
+   * Returns an array of ports that a message is carrying
+   * @returns {[]}
+   */
+  get ports () {
+    return this._opts.ports
+  }
+
+  /**
+   * Returns the messages payload
+   * @returns {ArrayBuffer}
+   */
+  get ticks () {
+    return this._opts.resources.ticks
+  }
+
+  /**
+   * Returns an array of ports that a message is carrying
+   * @returns {[]}
+   */
+  get priority () {
+    return this._opts.resources.priority
+  }
+
+  /**
+   * Returns the port from which the message arrived
    * @returns {*}
    */
-  get resources () {
-    return this._payload.resources
+  get fromPort () {
+    return this._fromPort
   }
 
   /**
--- tests/index.js
+++ tests/index.js
@@ -2,18 +2,25 @@ const tape = require('tape')
 const Message = require('../')
 
 tape('message API tests', async t => {
+  const aPort = Symbol('A')
+
   const params = {
-    to: '0/2/1/2',
-    data: ['test'],
-    resources: 77
+    data: 'test',
+    ports: [aPort],
+    resources: {
+      ticks: 77,
+      priority: 100
+    }
   }
+
   let message = new Message(params)
   message._fromPort = 'test'
 
   t.equals(message.fromPort, 'test', 'to getter should work')
-  t.equals(message.payload.to, params.to, 'to getter should work')
-  t.equals(message.payload.data, params.data, 'data getter should work')
-  t.equals(message.resources, 77, 'resources getter should work')
+  t.equals(message.ports[0], aPort, 'to getter should work')
+  t.equals(message.data, 'test', 'to getter should work')
+  t.equals(message.ticks, 77, 'resources getter should work')
+  t.equals(message.priority, 100, 'resources getter should work')
   t.equals(message.hops, 0, 'hops should return correctly')
   t.end()
 })
