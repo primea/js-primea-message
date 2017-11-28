fac5d09e1a3c31d627507ab4c09b78729128e96e
wanderer <mjbecze@gmail.com>
2017-11-28 12:08:17 -0500

    added hashing func

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events')
+const crypto = require('node-webcrypto-shim')
 
 /**
  * This implements Messages for Primea
@@ -31,6 +32,20 @@ module.exports = class Message extends EventEmitter {
 
   toJSON () {
     return this._opts
+  }
+
+  /**
+   * Gets the SHA-256 hash for some given data
+   * @param {Buffer} data - the data to be hashed
+   * @param {number} length - the number of bytes of the hash to return. must be <= 32
+   * @returns {Promise} resolves with 32 bytes of hashed data
+   */
+  static hash (data, length = 32) {
+    return crypto.subtle.digest({
+      name: 'SHA-256'
+    }, data).then(function (hash) {
+      return new Uint8Array(hash).subarray(0, length)
+    })
   }
 
   static get defaults () {
--- tests/index.js
+++ tests/index.js
@@ -22,5 +22,9 @@ tape('message API tests', async t => {
   t.equals(message.ticks, 0, 'resources getter should work')
   message.ticks = 99
   t.equals(message.ticks, 99, 'setter should work')
+
+  const hash = await Message.hash(Buffer.from('test'))
+  t.equal(Buffer.from(hash).toString('hex'), '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08')
+
   t.end()
 })
